41912e5a1a29fc47dd185d5cc7afbcea40c5aeab
leemove <923522454@qq.com>
2018-01-31 10:50:30 +0800

    update

---
+++ 4-复合类型数据/p4.1/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+func main() {
+	c1 := sha256.Sum256([]byte("x"))
+	c2 := sha256.Sum256([]byte("X"))
+	fmt.Printf("%x\n%x\n%t\n%T\n", c1, c2, c1 == c2, c1)
+	for i := range c1 {
+		fmt.Println(c1[i] == c2[i])
+	}
+}
