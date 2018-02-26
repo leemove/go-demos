0dc181a498162eebd5005aec5d78784f7b02fc86
leemove <923522454@qq.com>
2018-02-26 16:12:07 +0800

    update

--- 8-Goroutines和Channels/4-串联的Channels/main.go
+++ 8-Goroutines和Channels/4-串联的Channels/main.go
@@ -7,19 +7,25 @@ import (
 func main() {
 	naturals := make(chan int)
 	squares := make(chan int)
-	go func() {
-		for x := 0; x < 3; x++ {
-			naturals <- x
-		}
-		close(naturals)
-	}()
-	go func() {
-		for x := range naturals {
-			squares <- x * x
-		}
-		close(squares)
-	}()
-	for x := range squares {
-		fmt.Println(x)
+	go counter(naturals)
+	go squarer(squares, naturals)
+	printer(squares)
+}
+
+func counter(naturals chan<- int) {
+	for x := 0; x < 3; x++ {
+		naturals <- x
+	}
+	close(naturals)
+}
+func squarer(out chan<- int, in <-chan int) {
+	for v := range in {
+		out <- v * v
+	}
+	close(out)
+}
+func printer(in <-chan int) {
+	for v := range in {
+		fmt.Println(v)
 	}
 }
