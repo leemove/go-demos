51be6a752464c4286c8d57e52ae29787a7316caa
leemove <923522454@qq.com>
2018-02-28 14:57:00 +0800

    update

---
+++ 8-Goroutines和Channels/6-并发的web爬虫/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gopl.io/ch5/links"
+)
+
+var tokens = make(chan struct{}, 20)
+
+//!+crawl
+func crawl(url string) []string {
+	fmt.Println(url)
+	tokens <- struct{}{}
+	list, err := links.Extract(url)
+	<-tokens
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
+
+//!-crawl
+
+//!+main
+func main() {
+	worklist := make(chan []string)
+
+	// Start with the command-line arguments.
+	go func() { worklist <- os.Args[1:] }()
+
+	// Crawl the web concurrently.
+	seen := make(map[string]bool)
+	for list := range worklist {
+		for _, link := range list {
+			if !seen[link] {
+				seen[link] = true
+				go func(link string) {
+					worklist <- crawl(link)
+				}(link)
+			}
+		}
+	}
+}
