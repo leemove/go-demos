28c0b663faca8521e1444112d8f8a2cd2e1417a6
leemove <923522454@qq.com>
2018-03-06 09:51:07 +0800

    update

---
+++ 11-反射/7-获取结构体字段标识/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"gopl.io/ch12/params"
+)
+
+func main() {
+
+}
+
+func search(resp http.ResponseWriter, req *http.Request) {
+	var data struct {
+		Labels     []string `http:"l"`
+		MaxResults int      `http:"max"`
+		Exact      bool     `http:"x"`
+	}
+	data.MaxResults = 10
+	if err := params.Unpack(req, &data); err != nil {
+		http.Error(resp, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(resp, "Search: %+v\n", data)
+}
